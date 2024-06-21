59037f0b668a3ada3026a8dd2b1348df08a555b7
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-21 21:18:23 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -38,18 +38,16 @@ resource "aws_instance" "strapi_instance" {
   subnet_id = aws_subnet.public_subnet1.id
   associate_public_ip_address = true
   user_data                   = <<-EOF
-                                #!/bin/bash
-                                sudo apt update
-                                curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-                                sudo bash -E nodesource_setup.sh
-                                sudo apt update && sudo apt install nodejs -y
-                                sudo npm install -g yarn && sudo npm install -g pm2
-                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
-                                cd simple-strapi
-                                echo "const strapi = require('@strapi/strapi');
-                                strapi().start();" > server.js
-                                pm2 start server.js
-                                sleep 360
+              sudo apt-get update -y
+              sudo apt-get install -y nodejs npm git
+              sudo npm install -g pm2
+              sudo npm install -g strapi@latest
+              sudo mkdir -p /srv/strapi 
+              sudo chown ubuntu:ubuntu /srv/strapi
+             # "cd /srv/strapi",
+              if [ ! -d /srv/strapi ]; then sudo git clone https://github.com/haripriya2413/Strapi-CICD /srv/strapi; else cd /srv/strapi && sudo git pull origin main; fi",
+              cd /srv/strapi
+            
                                 EOF
 
 
