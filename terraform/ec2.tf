6403efc8f29160e2b6aeaef12542d03cb3ca0f77
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-21 18:56:17 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -37,6 +37,21 @@ resource "aws_instance" "strapi_instance" {
   vpc_security_group_ids      = [aws_security_group.strapi-sg.id]
   subnet_id = aws_subnet.public_subnet1.id
   associate_public_ip_address = true
+  user_data                   = <<-EOF
+                                #!/bin/bash
+                                sudo apt update
+                                curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+                                sudo bash -E nodesource_setup.sh
+                                sudo apt update && sudo apt install nodejs -y
+                                sudo npm install -g yarn && sudo npm install -g pm2
+                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
+                                #cd simple-strapi
+                                #echo "const strapi = require('@strapi/strapi');
+                                #strapi().start();" > server.js
+                                #pm2 start server.js
+                                #sleep 360
+                                EOF
+
 
 
   tags = {
