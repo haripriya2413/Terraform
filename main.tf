c9e99d0e5095fbde814a401e0b1e889c6906ecfa
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-21 10:13:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,77 +1,21 @@
-# Configure and downloading plugins for aws
-provider "aws" {
-  region     = "${var.aws_region}"
+resource "random_id" "this" {
+  byte_length = 8
 }
 
-# Creating VPC
-resource "aws_vpc" "demovpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "Demo VPC"
-  }
-}
-# Creating Internet Gateway 
-resource "aws_internet_gateway" "demogateway" {
-  vpc_id = "${aws_vpc.demovpc.id}"
+resource "tls_private_key" "strapi_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
 }
 
-# Grant the internet access to VPC by updating its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.demovpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.demogateway.id}"
+resource "aws_key_pair" "strapi_keypair" {
+  key_name   = "strapi-keypair-${random_id.this.hex}"
+  public_key = tls_private_key.strapi_key.public_key_openssh
 }
 
-# Creating 1st subnet 
-resource "aws_subnet" "demosubnet" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-security-group-${random_id.this.hex}"
+  description = "Security group for Strapi EC2 instance"
 
-  tags = {
-    Name = "Demo subnet"
-  }
-}
-
-# Creating 2nd subnet 
-resource "aws_subnet" "demosubnet1" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet1_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Demo subnet 1"
-  }
-}
-
-# Creating Security Group
-resource "aws_security_group" "demosg" {
-  name        = "Demo Security Group"
-  description = "Demo Module"
-  vpc_id      = "${aws_vpc.demovpc.id}"
-
-  # Inbound Rules
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTPS access from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH access from anywhere
   ingress {
     from_port   = 22
     to_port     = 22
@@ -79,108 +23,35 @@ resource "aws_security_group" "demosg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Splunk default port
   ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  # Replication Port
-  ingress {
-    from_port   = 8089
-    to_port     = 8089
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Management Port
-  ingress {
-    from_port   = 4598
-    to_port     = 4598
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Ingestion Port
-  ingress {
-    from_port   = 9997
-    to_port     = 9997
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound Rules
-  # Internet access to anywhere
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-# Creating key pair
-resource "aws_key_pair" "demokey" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key)}"
-}
-  
-# Creating EC2 Instance
-resource "aws_instance" "demoinstance" {
-
-  # AMI based on region 
-  ami = "${lookup(var.ami, var.aws_region)}"
-
-  # Launching instance into subnet 
-  subnet_id = "${aws_subnet.demosubnet.id}"
-
-  # Instance type 
-  instance_type = "${var.instancetype}"
-  
-  # Count of instance
-  count= "${var.master_count}"
-  
-  # SSH key that we have generated above for connection
-  key_name = "${aws_key_pair.demokey.id}"
-
-  # Attaching security group to our instance
-  vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
-
-  # Attaching Tag to Instance 
   tags = {
-    Name = "Search-Head-${count.index + 1}"
+    Name = "Strapi Security Group-${random_id.this.hex}"
   }
-  
-  # Root Block Storage
-  root_block_device {
-    volume_size = "40"
-    volume_type = "standard"
+}
+
+resource "aws_instance" "strapi_instance" {
+  ami           = var.ami
+  instance_type = "t2.medium"
+  key_name      = aws_key_pair.strapi_keypair.key_name
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  tags = {
+    Name = "StrapiInstance-${random_id.this.hex}"
   }
-  
-  #EBS Block Storage
-  ebs_block_device {
-    device_name = "/dev/sdb"
-    volume_size = "80"
-    volume_type = "standard"
-    delete_on_termination = false
-  }
-  
-  # SSH into instance 
-  connection {
-    
-    # Host name
-    host = self.public_ip
-    # The default username for our AMI
-    user = "ec2-user"
-    # Private key for connection
-    private_key = "${file(var.private_key)}"
-    # Type of connection
-    type = "ssh"
-  }
-  
-  # Installing splunk on newly created instance
+
  provisioner "remote-exec" {
     inline = [
              "sudo apt-get update -y",
@@ -211,4 +82,15 @@ resource "aws_instance" "demoinstance" {
             
     ]
   }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.strapi_key.private_key_pem
+    host        = self.public_ip
+  }
+}
+
+output "strapi_private_key" {
+  value = tls_private_key.strapi_key.private_key_pem
+  sensitive = true
 }
