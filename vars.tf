5fb7f5398f6351f3a65811c1fc9ace77f8b034d4
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-21 10:15:49 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,38 +1,12 @@
-# Defining Public Key
-variable "public_key" {
-  default = "tests.pub"
-}
 
-# Defining Private Key
-variable "private_key" {
-  default = "tests.pem"
-}
-
-# Definign Key Name for connection
-variable "key_name" {
-  default = "tests"
-  description = "Desired name of AWS key pair"
-}
 
 # Defining Region
 variable "aws_region" {
   default = "us-east-1"
 }
 
-# Defining CIDR Block for VPC
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
 
-# Defining CIDR Block for Subnet
-variable "subnet_cidr" {
-  default = "10.0.1.0/24"
-}
 
-# Defining CIDR Block for 2d Subnet
-variable "subnet1_cidr" {
-  default = "10.0.2.0/24"
-}
 
 # Defining AMI
 variable "ami" {
@@ -41,12 +15,4 @@ variable "ami" {
   }
 }
 
-# Defining Instace Type
-variable "instancetype" {
-  default = "t2.small"
-}
 
-# Defining Master count 
-variable "master_count" {
-  default = 1
-}
