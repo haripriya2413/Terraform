a2f5a8a416f4ced54f959f56c05d4ea5e573cad0
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-21 22:55:24 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -45,12 +45,7 @@ resource "aws_instance" "strapi_instance" {
                                 sudo bash -E nodesource_setup.sh
                                 sudo apt update && sudo apt install nodejs -y
                                 sudo npm install -g yarn && sudo npm install -g pm2
-                                echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
-                                cd simple-strapi
-                                echo "const strapi = require('@strapi/strapi');
-                                strapi().start();" > server.js
-                                pm2 start server.js
-                                sleep 360
+                               
                                 EOF
                              
 
